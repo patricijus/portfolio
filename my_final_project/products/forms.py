6112f6db87952604970f9b6883ab436b7f411136
patricijus <ivandimic@hotmail.com>
2022-12-14 17:21:50 +1100

    Added product update functionality

--- my_final_project/products/forms.py
+++ my_final_project/products/forms.py
@@ -4,3 +4,5 @@ from wtforms.validators import DataRequired
 
 class ProductForm(FlaskForm):
     name = StringField('Product Name', validators=[DataRequired()])
+    internal_code = StringField('Internal Code', validators=[DataRequired()])
+    customer_code = StringField('Customer Code', validators=[DataRequired()])
--- my_final_project/products/views.py
+++ my_final_project/products/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, redirect, url_for
+from flask import render_template, Blueprint, redirect, request, url_for
 from .models import Product
 from .forms import ProductForm
 from my_final_project import db
@@ -20,18 +20,32 @@ def add_product():
     form = ProductForm()
     error = None
     if form.validate_on_submit():
-        try:
-            product = Product(name=form.name.data)
-            print(product)
-            db.session.add(product)
-            db.session.commit()
-            return redirect(url_for('products.product_list'))
-        except:
-            error = "Duplicate"
-            return render_template('product-add.html', form=form, error=error)
+        
+        product = Product(
+            name=form.name.data,
+            customer_code=form.customer_code.data,
+            internal_code=form.internal_code.data)
+            
+        db.session.add(product)
+        db.session.commit()
+        return redirect(url_for('products.product_list'))
+
     return render_template('product-add.html', form=form)
 
-@bp.route('/edit_product/<int:id>')
+@bp.route('/edit_product/<int:id>', methods=['GET','POST'])
 def edit_product(id):
+    form = ProductForm()
+    product = Product.query.filter_by(id=id).first()
 
-    return "Future edit product"
+    if form.validate_on_submit():
+        product.name = form.name.data
+        product.customer_code = form.customer_code.data
+        product.internal_code = form.internal_code.data
+        db.session.commit()
+        return redirect(url_for('products.product_list'))
+    if request.method == 'GET':
+        form.name.data = product.name
+        form.customer_code.data = product.customer_code
+        form.internal_code.data = product.internal_code
+        return render_template('product-add.html', form=form)
+    return redirect(url_for('products.product_list'))    
