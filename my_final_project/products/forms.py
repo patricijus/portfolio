408c8dff088047a4c1aa13b442fccf6551375d85
patricijus <ivandimic@hotmail.com>
2022-12-15 18:08:05 +1100

    Added full crud to products app

--- my_final_project/products/forms.py
+++ my_final_project/products/forms.py
@@ -5,9 +5,11 @@ from wtforms.validators import DataRequired
 
 class ProductForm(FlaskForm):
     
-
-    
     name = StringField('Product Name', validators=[DataRequired()])
     internal_code = StringField('Internal Code', validators=[DataRequired()])
     customer_code = StringField('Customer Code', validators=[DataRequired()])
     category_id = SelectField(choices=[])
+
+
+class ProductCategoryForm(FlaskForm):
+    name= StringField('Category Name', validators=[DataRequired()])
